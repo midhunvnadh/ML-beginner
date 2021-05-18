ca4c682562bfdd173f97007fbe0ba965b0772e2a
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-05-18 18:34:06 +0530

    Accuracy 6.805

--- start.py
+++ start.py
@@ -3,38 +3,36 @@ from numpy.lib.function_base import append
 from functions import *
 import pandas as pd
 import numpy as np
-import math
 
 test_data = pd.read_csv('Test.csv')
 train_data = pd.read_csv('Train.csv')
 
 def printAnim(i, j):
     bar = [
-    "[        ]",
-    "[=       ]",
-    "[===     ]",
-    "[====    ]",
-    "[=====   ]",
-    "[======  ]",
-    "[======= ]",
-    "[========]",
-    "[ =======]",
-    "[  ======]",
-    "[   =====]",
-    "[    ====]",
-    "[     ===]",
-    "[      ==]",
-    "[       =]",
-    "[        ]",
-    "[        ]"
+        "[        ]",
+        "[=       ]",
+        "[===     ]",
+        "[====    ]",
+        "[=====   ]",
+        "[======  ]",
+        "[======= ]",
+        "[========]",
+        "[ =======]",
+        "[  ======]",
+        "[   =====]",
+        "[    ====]",
+        "[     ===]",
+        "[      ==]",
+        "[       =]",
+        "[        ]",
+        "[        ]"
     ]
     print(" {} Creating data {}/{} ".format(bar[i%len(bar)], i, j), end = "\r")
 
-def finalizeData(list, id, fam_size, i, j):
+def finalizeData(list, id, i, j):
     mode = list["Segmentation"].mode()
     list = list.query("Segmentation in @mode").sort_values("Family_Size")
-    index = 0
-    segmentation = list["Segmentation"].iloc[index]
+    segmentation = list["Segmentation"].iloc[0]
     data.append([id, segmentation])
     printAnim(i, j)
 
@@ -54,29 +52,23 @@ def startAnalysis():
             'Family_Size':test_data["Family_Size"][i],
             'Var_1':test_data["Var_1"][i],
         }
-        fns = [
-            {"metric":"Age", "Age": person["Age"], "min":person["Age"] - 3, "max":person["Age"] + 3},
-            {"metric":"Work_Experience", "Work_Experience": person["Work_Experience"], "min":person["Work_Experience"] - 3, "max":person["Work_Experience"] + 3},
-            {"metric":"Family_Size", "Family_Size": person["Family_Size"], "min":person["Family_Size"] - 3, "max":person["Family_Size"] + 3}
-        ]
-        for x in ["Gender", "Ever_Married", "Graduated", "Profession", "Spending_Score", "Var_1", "Family_Size", "Work_Experience"]:
+        for x in ["Gender", "Ever_Married", "Graduated", "Profession", "Spending_Score", "Var_1"]:
             if(length(final_data) > 0):
                 tmp_final = final_data.query('{} == "{}"'.format(x, person[x]))
                 if(length(tmp_final) > 0):
                     final_data = tmp_final
                 else:
                     continue
-
-        for x in fns:
+        gi = 3
+        for x in ["Age", "Family_Size", "Work_Experience"]:
             if(length(final_data) > 0):
-                max = (0) if math.isnan(x["max"]) else x["max"]
-                min = (0) if math.isnan(x["min"]) else x["max"]
-                tmp_data = final_data.query("{} <= {} & {} >= {}".format(x["metric"], max, x["metric"], min))
+                tmp_final = final_data.iloc[(final_data[x]-person[x]).abs().argsort()[:gi]]
                 if(length(tmp_final) > 0):
                     final_data = tmp_final
                 else:
-                    continue;
-        finalizeData(final_data, person["ID"], person['Family_Size'], i, rows)
+                    continue
+            gi = gi - 1
+        finalizeData(final_data, person["ID"], i, rows)
     
 data = []
 startAnalysis()
